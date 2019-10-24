1084b082da5a7f5ba36a484b724a2784991723d8
Florian Magot <flo.magot@gmail.com>
2019-10-24 20:34:31 +0100

    [FIX] 0.9.8 add query params to upload()

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -1,4 +1,5 @@
 import fetch from 'cross-fetch';
+import qs from 'qs';
 import * as JSONAPI from 'jsonapi-typescript';
 import { BulbThings } from '../../src';
 import { RequestOptions } from '../interfaces/request-options';
@@ -6,10 +7,26 @@ import { RequestOptions } from '../interfaces/request-options';
 export const upload = async (
     bulb: BulbThings,
     url: string,
-    options: { data: any; file: any; params?: RequestOptions }
+    options: { data?: any; file?: any; params?: RequestOptions } = {}
 ) => {
-    const apiToken =
-        (options.params && options.params.apiToken) || bulb.options.apiToken;
+    options.params = Object.assign(
+        {},
+        bulb.options.companyId
+            ? { companyId: bulb.options.companyId }
+            : undefined,
+        options.params
+    );
+
+    const apiToken = options.params.apiToken || bulb.options.apiToken;
+
+    // Avoid leaking the token in the URL
+    delete options.params.apiToken;
+
+    if (Object.keys(options.params).length) {
+        url = `${url}?${qs.stringify(options.params, {
+            arrayFormat: 'comma'
+        })}`;
+    }
 
     const body = new FormData();
     body.append('file', options.file);
