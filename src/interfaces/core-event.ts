37d0a49ee3d9b51ae8c91a94b47d07b63ba8d2a2
Florian Magot <flo.magot@gmail.com>
2020-06-03 17:56:42 +0100

    [ADD][0.11.8] keyId to core events

--- src/interfaces/core-event.ts
+++ src/interfaces/core-event.ts
@@ -3,6 +3,7 @@ import { CoreEventType } from '../types/core-event-type';
 export interface CoreEvent {
     type: CoreEventType;
     data: {
+        keyId?: string;
         resource: any;
         previousData?: any;
     };
