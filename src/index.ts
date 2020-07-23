f1642a7a925143ad5db0b62a1087a2b0a81c945e
Florian Magot <flo.magot@gmail.com>
2020-07-23 14:08:01 +0100

    [DELETE] unused import

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,6 @@ import { CoreEventType } from './types/core-event-type';
 import { CoreEvent } from './interfaces/core-event';
 import { BulbThingsOptions } from './interfaces/bulbthings-options';
 import { UiResource } from './resources/ui';
-import { UiNode } from './interfaces/ui/ui-node';
 
 // Export JSONAPI Error class to parse errors
 export { DocWithErrors as ApiError } from 'jsonapi-typescript';
