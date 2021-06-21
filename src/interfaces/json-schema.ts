f062daea0ce1cffc7d58f40f5a52d576bbb3dbd8
Robert Parasca <robert.parasca@bulbthings.com>
2021-06-21 14:11:20 +0300

    feat: Added new properties on the JsonSchema interface

--- src/interfaces/json-schema.ts
+++ src/interfaces/json-schema.ts
@@ -1,3 +1,6 @@
 import { JSONSchema7 } from 'json-schema';
 
-export interface JsonSchema extends JSONSchema7 {}
+export interface JsonSchema extends JSONSchema7 {
+    inputType?: string;
+    mediaTypes?: string[];
+}
