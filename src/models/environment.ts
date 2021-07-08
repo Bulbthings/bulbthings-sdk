3b2b54a32bc9aa45336296ca56de6a7d39464e17
Florian Magot <flo.magot@gmail.com>
2021-07-08 19:47:18 +0100

    feat: update environment model

--- src/models/environment.ts
+++ src/models/environment.ts
@@ -8,16 +8,28 @@ import { JsonApiModelConfig } from '../decorators/json-api-model';
 export class Environment extends JsonApiModel<Environment> {
     @Attribute()
     config: {
+        androidAppUrl?: string;
+        emailTemplate?: string;
         favicon16?: string;
         favicon32?: string;
         faviconAppleTouch?: string;
         faviconIco?: string;
         faviconSafariPinnedTab?: string;
+        iosAppUrl?: string;
+        logo: string;
         longTitle?: string;
         masterAccountAvatar?: string;
         masterAccountLabel?: string;
+        mobileBrowserDisclaimer?: {
+            content?: string;
+            title?: string;
+        };
         primaryColor: string;
+        primaryColorVariant?: string;
+        secondaryColor?: string;
+        secondaryColorVariant?: string;
         title: string;
         webmanifest?: string;
+        webUrl: string;
     };
 }
