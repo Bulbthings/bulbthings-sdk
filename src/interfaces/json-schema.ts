0a2b6c1c38b2f2b9635184d31f93cc41c5ff1cf2
Robert Parasca <robert.parasca@bulbthings.com>
2021-08-03 11:51:34 +0300

    feat: Added flag for progress events

--- src/interfaces/json-schema.ts
+++ src/interfaces/json-schema.ts
@@ -3,4 +3,5 @@ import { JSONSchema7 } from 'json-schema';
 export interface JsonSchema extends JSONSchema7 {
     inputType?: 'file' | 'textarea';
     mediaTypes?: string[];
+    isProgressEvent?: boolean;
 }
