1547f39cc1955aae406c015798bf54256daf7f45
Florian Magot <flo.magot@gmail.com>
2021-08-04 16:07:03 +0100

    feat: add acknowledgement events

--- src/interfaces/json-schema.ts
+++ src/interfaces/json-schema.ts
@@ -3,5 +3,4 @@ import { JSONSchema7 } from 'json-schema';
 export interface JsonSchema extends JSONSchema7 {
     inputType?: 'file' | 'textarea';
     mediaTypes?: string[];
-    isProgressEvent?: boolean;
 }
