0e14ce765ea709c245367cc4b2ba6db3ab3ff637
Florian Magot <flo.magot@gmail.com>
2023-12-27 16:23:29 +0000

    feat(applus): add new values for schema.inputType

--- src/interfaces/json-schema.ts
+++ src/interfaces/json-schema.ts
@@ -1,6 +1,6 @@
 import { JSONSchema7 } from 'json-schema';
 
 export interface JsonSchema extends JSONSchema7 {
-    inputType?: 'file' | 'textarea';
+    inputType?: 'file' | 'textarea' | 'entity' | 'entityType';
     mediaTypes?: string[];
 }
