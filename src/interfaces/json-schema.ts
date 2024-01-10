67e000aabefa90868f86c8898487b465a83b868c
Florian Magot <flo.magot@gmail.com>
2024-01-10 14:49:32 +0000

    feat: update interfaces/json-schema

--- src/interfaces/json-schema.ts
+++ src/interfaces/json-schema.ts
@@ -3,4 +3,18 @@ import { JSONSchema7 } from 'json-schema';
 export interface JsonSchema extends JSONSchema7 {
     inputType?: 'file' | 'textarea' | 'entity' | 'entityType';
     mediaTypes?: string[];
+    propertyOrder?: string[];
+    display?: {
+        icon?: { fontAwesome?: string[] | string };
+        numberFormat?: {
+            maximumFractionDigits?: number;
+            minimumFractionDigits?: number;
+        };
+        ranges?: {
+            displayValue?: any;
+            schema?: JsonSchema;
+            style?: { backgroundColor?: string; color?: string };
+        }[];
+        style?: { backgroundColor?: string; color?: string };
+    };
 }
