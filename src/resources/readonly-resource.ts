971e7c95095d2548c9f7d8c3b120d2ff13475b37
Florian Magot <flo.magot@gmail.com>
2025-02-05 12:36:59 +0000

    feat: add getCached() to ReadonlyResource class

--- src/resources/readonly-resource.ts
+++ src/resources/readonly-resource.ts
@@ -1,9 +1,10 @@
+import { ApiError, Bulbthings } from '..';
+import { JsonApiModelConfig } from '../interfaces/json-api-model-config';
 import { JsonApiOptions } from '../interfaces/json-api-options';
 import { JsonApiModel } from '../models/jsonapi-model';
 import { ModelType } from '../types/model-type';
 import { findAll } from '../utils/find-all';
 import { findById } from '../utils/find-by-id';
-import { Bulbthings } from '..';
 
 export class ReadonlyResource<T extends JsonApiModel<T>> {
     constructor(
@@ -34,5 +35,39 @@ export class ReadonlyResource<T extends JsonApiModel<T>> {
 
     async findById(id: string, options?: JsonApiOptions): Promise<T> {
         return findById(this.bulbthings, this.modelType, id, options);
+    }
+
+    async getCached(id: string, options?: JsonApiOptions): Promise<T> {
+        const endpoint = this.getEndpoint();
+        let cached = this.bulbthings.cache.get<T>(endpoint, id);
+        if (cached === undefined) {
+            const mId = `${endpoint}-${id}`;
+            const release = await this.bulbthings.cache.getMutex(mId).acquire();
+            try {
+                cached =
+                    this.bulbthings.cache.get(endpoint, id) ??
+                    // findById will update the cache value
+                    (await this.findById(id, options));
+            } catch (err) {
+                const status = (err as ApiError)?.errors?.[0]?.status;
+                if (status === '404') {
+                    this.bulbthings.cache.set(endpoint, id, null);
+                } else {
+                    console.error(`[${endpoint}][getCached]`, err);
+                }
+            } finally {
+                release();
+            }
+        }
+        return cached;
+    }
+
+    private getEndpoint() {
+        return (
+            Reflect.getMetadata(
+                'JsonApiModelConfig',
+                this.modelType
+            ) as JsonApiModelConfig
+        ).endpoint;
     }
 }
